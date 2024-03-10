6e5e0621f2de82be97115280fac831ebcd1ca45c
hems-07 <hemanthpalani001@gmail.com>
2024-03-10 18:55:44 +0530

    First commit SFDS

---
+++ fastapi-backend/main.py
@@ -0,0 +1,140 @@
+from fastapi import FastAPI,File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras 
+from PIL import Image,ImageOps
+import pickle
+import cv2
+import uvicorn
+#import seaborn as sns
+app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+def load_algo_model():
+    loaded_model = pickle.load(open('D:/CEG/Machine Learning/Project/Signature_Forgery_Detection/models/logregmodel.hdf5','rb'))
+    return loaded_model
+
+"""
+file_path = 'C:/Users/prahm/Downloads/cnn_model.hdf5'
+
+try:
+    model = tf.keras.models.load_model(file_path)
+except Exception as e:
+    print(f"Error: {e}")
+"""
+def load_ann_model():
+    model = tf.keras.models.load_model(r'D:/CEG/Machine Learning/Project/Signature_Forgery_Detection/models/ann_sign_model.hdf5', compile=False)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+def load_cnn_model():
+    model = tf.keras.models.load_model(r'D:/CEG/Machine Learning/Project/Signature_Forgery_Detection/models/cnn_sign_model.hdf5', compile=False)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+cnn_model =load_cnn_model()    
+ann_model = load_ann_model()
+algo_model = load_algo_model()
+
+@app.post("/api/upload/algo")
+async def upload_file(file: UploadFile = File(...)):
+    file_size_mb = len(file.file.read()) / (1024 * 1024)
+    file.file.seek(0)  # Move the file pointer back to the beginning of the file
+
+    print(f"Received file: {file.filename}, Content-Type: {file.content_type}, Size: {file_size_mb:.2f} MB")
+    pil_image = Image.open(file.file)
+
+    # Converting the PIL image to a numpy array
+    cur_img = np.array(pil_image)
+    
+    cur_img = cv2.cvtColor(cur_img,cv2.COLOR_BGR2RGB)
+    cur_img = cv2.resize(cur_img,(224,224))
+    cur_img=cur_img/255
+    cur_img = cur_img.reshape((1, 224, 224, 3))
+    cur_img = cur_img.reshape((1, -1))
+    predcur=algo_model.predict(cur_img)
+    message = ""
+    
+    #a=np.argmax(algo_model.predict(cur_img), axis=1)
+    if(predcur==1):
+        message = "The signature has been forged"
+    else:
+        message = "The signature is real"
+    return JSONResponse(content={"message": message}, status_code=200)
+
+@app.post("/api/upload/ann")
+async def upload_file(file: UploadFile = File(...)):
+    file_size_mb = len(file.file.read()) / (1024 * 1024)
+    file.file.seek(0)  # Move the file pointer back to the beginning of the file
+
+    print(f"Received file: {file.filename}, Content-Type: {file.content_type}, Size: {file_size_mb:.2f} MB")
+    pil_image = Image.open(file.file)
+
+    # Converting the PIL image to a numpy array
+    cur_img = np.array(pil_image)
+    
+    cur_img = cv2.cvtColor(cur_img,cv2.COLOR_BGR2RGB)
+    cur_img = cv2.resize(cur_img,(224,224))
+    cur_img=cur_img/255
+    cur_img = cur_img.reshape((1, 224, 224, 3))
+    predcur=ann_model.predict(cur_img)
+    message = ""
+    a=np.argmax(ann_model.predict(cur_img), axis=1)
+    if(a==0):
+        message = "The signature is real"
+    else:
+        message = "The signature has been forged"
+    predcur=predcur*100
+    predcur = np.round(predcur,decimals=2)
+    return JSONResponse(content={"message": message,"prediction": predcur.tolist()}, status_code=200)
+@app.post("/api/upload/cnn")
+async def upload_file(file: UploadFile = File(...)):
+    file_size_mb = len(file.file.read()) / (1024 * 1024)
+    file.file.seek(0)  # Move the file pointer back to the beginning of the file
+
+    print(f"Received file: {file.filename}, Content-Type: {file.content_type}, Size: {file_size_mb:.2f} MB")
+    pil_image = Image.open(file.file)
+
+    # Convert the PIL image to a numpy array
+    cur_img = np.array(pil_image)
+    
+    cur_img = cv2.cvtColor(cur_img,cv2.COLOR_BGR2RGB)
+    cur_img = cv2.resize(cur_img,(224,224))
+    cur_img=cur_img/255
+    cur_img = cur_img.reshape((1, 224, 224, 3))
+    predcur=cnn_model.predict(cur_img)
+    message = ""
+    a=np.argmax(cnn_model.predict(cur_img), axis=1)
+    if(a==0):
+        message = "The signature is real"
+    else:
+        message = "The signature has been forged"
+    
+    predcur=predcur*100
+    
+    predcur = np.round(predcur,decimals=2)
+    #print(predcur)
+    return JSONResponse(content={"message": message,"prediction": predcur.tolist()}, status_code=200)
+
+
+@app.get("/")
+async def curmain():
+    return {'data': 'full stack application using fastapi'}
---
+++ frontend/src/Home.jsx
@@ -0,0 +1,36 @@
+import React,{useState,useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
+import './Home.css'
+import Button from './Button';
+import StyledText from './StyledText';
+function Home() {
+    const navigate = useNavigate()
+const handleClickAlgo = ()=>{
+    navigate('/algo')
+}
+
+const handleClickANN= ()=>{
+    navigate('/ann')
+}
+
+const handleClickCNN = ()=>{
+    navigate('/cnn')
+}
+  return (
+    <>
+    
+    <div className='p-4'>
+        
+        <StyledText text= "SIGNATURE FORGERY DETECTION"/>
+    </div>
+    <div className="flex space-x-36">
+    
+    <Button text = "LOGISTIC REGRESSION" color = "#0FF0FC" onClick={handleClickAlgo}/>
+    <Button text = "ARTIFICIAL NEURAL NETWORK" color = "#f44c4c" onClick={handleClickANN}/>
+    <Button text = "CONVOLUTIONAL NEURAL NETWORK" color = "#39FF14" onClick={handleClickCNN}/>
+    </div>
+    </>
+  )
+}
+
+export default Home
